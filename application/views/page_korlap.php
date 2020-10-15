fbbfff803696fdd4733d874172002a2d0f654ba7
irfanltf <iluthfi469@gmail.com>
2020-10-15 07:57:40 +0700

    V.3

--- application/views/page_korlap.php
+++ application/views/page_korlap.php
@@ -1,22 +1,26 @@
 
+		<div class="row mt-5">
+			<div class="col-12 mx-auto">
 
+<div class="card text-center">
+  <div class="card-header">
     
+  </div>
+  <div class="card-body">
+    <h5 class="card-title">Welcome to your page!</h5>
+    <p class="card-text">
+      <ul class="list-group list-group-flush">
+    <li class="list-group-item list-group-item-primary">Name : <strong><?php echo $this->session->userdata('name') ?></strong></li>
+    <!-- <li class="list-group-item list-group-item-primary">Username : <strong><?php echo $this->session->userdata('username') ?></strong></li> -->
+    <li class="list-group-item list-group-item-primary">Level : <strong><?php echo $this->session->userdata('level') ?></strong></li>
+  </ul>
+    </p>
+    <a href="" class="btn btn-primary">Go</a>
+  </div>
+  <div class="card-footer text-muted">
+   
+  </div>
+</div>
 
-		<div class="row">
-			<div class="col-sm-9 col-md-7 col-lg-5 mx-auto">
-				<div class="card card-signin my-5">
-          			<div class="card-body">
-						<h3 class="card-title text-center">Welcome to your page!</h3>
-						<ul>
-							<li>Name : <?php echo $this->session->userdata('name') ?></li>
-							<li>Username : <?php echo $this->session->userdata('username') ?></li>
-							<li>Level : <?php echo $this->session->userdata('level') ?></li>
-						</ul>
-						
-					</div>
-				</div>
 			</div>
 		</div>
-	</div>
-</body>
-
